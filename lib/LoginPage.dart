0bf94a36f9f573a8dd7327756959caae3b598bad
Nguyễn Thọ Quân <thoquan150903@gmail.com>
2024-03-31 21:54:42 +0700

    Login

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -24,74 +24,206 @@ class LoginState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView(
-          padding: const EdgeInsets.all(16),
-          itemExtent: 70.0,
-          shrinkWrap: true,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
               children: [
-                Container(
-                  width: 160,
-                  padding: const EdgeInsets.all(6),
-                  child: const Text("Tên đăng nhập", style: TextStyle(fontSize: 20),),
-                ),
-                Expanded(child: TextField(
-                  controller: userNameController,
-                  keyboardType: TextInputType.text,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Nhập vào tên đăng nhập',
-                  ),))
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 160,
-                  padding: const EdgeInsets.all(6),
-                  child: const Text("Mật khẩu", style: TextStyle(fontSize: 20),),
-                ),
-                Expanded(child: TextField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Nhập vào mật khẩu',
-                  ),))
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
-                  onPressed: () async {
-                    final prefs = await SharedPreferences.getInstance();
-                    prefs.setString('username', userNameController.text).then((value) {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => HomePage())
-                      );
-                    });
-                  },
-                  child: const Text('Đăng nhập'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(context, "/register");
-                    },
-                    child: const Text("Đăng ký", style: TextStyle(fontSize: 20, color: Colors.white),),
-                  ),
+                const Text("Đăng nhập", style: TextStyle(fontSize: 25)),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5)
+                            )
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 8, bottom: 8, left: 80),
+                          child: Row(
+                            children: [
+                              Image.network(
+                                'https://i.pinimg.com/564x/d8/ff/30/d8ff3064a3067ac45a70f762f4977d90.jpg',
+                                width: 18,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 30),
+                                child: Text("KẾT NỐI VỚI FACEBOOK", style: TextStyle(color: Colors.blueAccent,),),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5)
+                            )
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 8, bottom: 8, left: 80),
+                          child: Row(
+                            children: [
+                              Image.network(
+                                'https://i.pinimg.com/736x/67/ca/ed/67caed418bc1332a490d35bc3ab25117.jpg',
+                                width: 18,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 36),
+                                child: Text("KẾT NỐI VỚI GOOGLE", style: TextStyle(color: Colors.redAccent,),),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5)
+                            )
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 8, bottom: 8, left: 80),
+                          child: Row(
+                            children: [
+                              Image.network(
+                                'https://i.pinimg.com/564x/cf/11/eb/cf11ebcc0a874e3ad3830431f7b0ab58.jpg',
+                                width: 18,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 31),
+                                child: Text("ĐĂNG NHẬP VỚI APPLE", style: TextStyle(color: Colors.black,),),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 15, bottom: 80),
+                      child: Text(
+                        "Chúng tôi sẽ không đăng thông tin mà bạn không có sự cho phép của bạn.",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white54,
+                        ),
+                      ),
+                    ),
+                    Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 14.0),
+                          child: Container(
+                            height: 1,
+                            color: Colors.white54,
+                          ),
+                        ),
+                        const Center(
+                          child: Card(
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 4.0, right: 4.0),
+                                child: Text("HOẶC", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white54),),
+                              )
+                          ),
+                        )
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                              child: TextField(
+                                keyboardType: TextInputType.text,
+                                decoration: InputDecoration(
+                                  labelText: 'Email',
+                                ),
+                              )
+                          )
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                              child: TextField(
+                                keyboardType: TextInputType.text,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Mật khẩu',
+                                ),
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Container(
+                        width: 420,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)
+                            )
+                          ),
+                          onPressed: () async {
+                            final prefs = await SharedPreferences.getInstance();
+                            prefs.setString('username', userNameController.text).then((value) {
+                              Navigator.of(context).push(
+                                  MaterialPageRoute(builder: (context) => HomePage())
+                              );
+                            });
+                          },
+                          child: const Text('ĐĂNG NHẬP', style: TextStyle(color: Colors.white),),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, "/register");
+                            },
+                            child: const Text("Đăng ký", style: TextStyle(color: Colors.green),),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 260),
+                            child: InkWell(
+                              onTap: () {},
+                              child: Text('Quên mật khẩu?', style: TextStyle(color: Colors.green),),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
