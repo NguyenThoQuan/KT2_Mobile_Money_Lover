56c547f5b1b781dafa46bd0c829223dad6df919e
Nguyễn Thọ Quân <thoquan150903@gmail.com>
2024-04-02 22:59:42 +0700

    UI 90%

---
+++ lib/NewTransaction.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatefulWidget {
+  const NewTransaction({super.key});
+
+  @override
+  State<StatefulWidget> createState() => NewTransactionState();
+}
+
+class NewTransactionState extends State<NewTransaction> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Thêm giao dịch'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.monetization_on_outlined),
+                      Text("   "),
+                      Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "0"
+                            ),
+                          )
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Row(
+                      children: [
+                        Icon(Icons.menu),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text("Chọn nhóm", style: TextStyle(color: Colors.white54, fontSize: 20),)
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Row(
+                      children: [
+                        Icon(Icons.edit_note),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text("Thêm ghi chú", style: TextStyle(color: Colors.white54),)
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Row(
+                      children: [
+                        Icon(Icons.date_range, color: Colors.white,),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text("Hôm nay", style: TextStyle(color: Colors.white),)
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Row(
+                      children: [
+                        Icon(Icons.account_balance_wallet, color: Colors.white,),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text("Tiền mặt", style: TextStyle(color: Colors.white),)
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: Container(
+        width: 465,
+        height: 40,
+        child: FloatingActionButton(
+          onPressed: () {  },
+          child: Text("Lưu"),
+          backgroundColor: Colors.green,
+        ),
+      ),
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'LoginPage.dart';
 import 'RegisterPage.dart';
-
-const Color darkBlue = Colors.black87;
+import 'NewTransaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: darkBlue,
+          scaffoldBackgroundColor: Colors.black87,
         ),
         initialRoute: '/login',
         routes: {
@@ -38,20 +37,21 @@ class HomePage extends StatefulWidget {
 
 class HomeState extends State<HomePage> {
   final GlobalKey _tooltipKey = GlobalKey();
+  double sizeInkWell = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(18.0),
+          padding: const EdgeInsets.all(18.0),
           child: Column(
             children: [
               Row(
                 children: [
-                  Text("0 đ", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),),
+                  const Text("0 đ", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),),
                   IconButton(
-                    icon: Icon(Icons.remove_red_eye, color: Colors.white,),
+                    icon: const Icon(Icons.remove_red_eye, color: Colors.white,),
                     iconSize: 18,
                     onPressed: () {},
                   ),
@@ -59,16 +59,17 @@ class HomeState extends State<HomePage> {
                     padding: const EdgeInsets.only(left: 347.5),
                     child: IconButton(
                         onPressed: () {},
-                        icon: Icon(Icons.notifications, color: Colors.white,)
+                        icon: const Icon(Icons.notifications, color: Colors.white,)
                     ),
                   )
                 ],
               ),
               Row(
                 children: [
-                  Text("Tổng số dư", style: TextStyle(fontSize: 12),),
+                  const Text("Tổng số dư", style: TextStyle(fontSize: 12, color: Colors.white54),),
                   Tooltip(
                     key: _tooltipKey,
+                    message: "Được tính dựa trên số dư của các ví nằm trong tổng",
                       child: IconButton(
                         onPressed: () {
                           final dynamic tooltip = _tooltipKey.currentState;
@@ -77,23 +78,22 @@ class HomeState extends State<HomePage> {
                         icon: Icon(Icons.help,),
                         iconSize: 14,
                       ),
-                    message: "Được tính dựa trên số dư của các ví nằm trong tổng",
                   ),
                 ],
               ),
               Card(
                 child: Padding(
-                  padding: EdgeInsets.only(top: 8, bottom: 8, left: 18, right: 18),
+                  padding: const EdgeInsets.only(top: 8, bottom: 8, left: 18, right: 18),
                   child: Column(
                     children: [
                       Row(
                         mainAxisSize: MainAxisSize.max,
                         children: [
-                          Text("Ví của tôi", textAlign: TextAlign.start, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                          const Text("Ví của tôi", textAlign: TextAlign.start, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
                           Padding(
-                            padding: const EdgeInsets.only(left: 269.3),
+                            padding: const EdgeInsets.only(left: 267.7),
                             child: TextButton(onPressed: () {},
-                                child: Text("Xem tất cả", style: TextStyle(color: Colors.green),)
+                                child: const Text("Xem tất cả", style: TextStyle(color: Colors.green),)
                             ),
                           )
                         ],
@@ -113,22 +113,197 @@ class HomeState extends State<HomePage> {
                                 'assets/tienmat.png',
                               width: 36,
                             ),
-                            Text("     Tiền mặt", style: TextStyle(color: Colors.white),),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 277),
+                            const Text("     Tiền mặt", style: TextStyle(color: Colors.white),),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 277),
                               child: Text("0 đ", style: TextStyle(color: Colors.white),),
                             )
                           ],
                         ),
-                      )
+                      ),
                     ],
                   ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Text("Báo cáo chi tiêu", style: TextStyle(color: Colors.white54),),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 254),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text("Xem báo cáo", style: TextStyle(color: Colors.green),),
+                          ),
+                        )
+                      ],
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Card(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  TextButton(
+                                      onPressed: () {},
+                                      child: const Text('Tuần', style: TextStyle(color: Colors.white54),)
+                                  ),
+                                  TextButton(
+                                      onPressed: () {},
+                                      child: const Text('Tháng', style: TextStyle(color: Colors.white54),)
+                                  )
+                                ],
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 16),
+                              child: Text("0 đ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
+                            ),
+                            const Row(
+                              children: [
+                                Text("Tổng chi tuần này ", style: TextStyle(color: Colors.white54),),
+                                Text("- 0%", style: TextStyle(color: Colors.yellow),)
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(36.0),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 200,
+                                    decoration: const BoxDecoration(
+                                      border: Border(
+                                        bottom: BorderSide(
+                                          color: Colors.white,
+                                          width: 1
+                                        )
+                                      )
+                                    ),
+                                  ),
+                                  const Padding(
+                                    padding: EdgeInsets.only(top: 8),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                      children: [
+                                        Text("Tuần trước", style: TextStyle(color: Colors.grey),),
+                                        Text("Tuần này", style: TextStyle(color: Colors.grey),)
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            const Text("Chi tiêu nhiều nhất", style: TextStyle(color: Colors.grey),),
+                            const Center(child: Padding(
+                              padding: EdgeInsets.only(top: 18, bottom: 18),
+                              child: Text("Các khoản chi tiêu sẽ được hiển thị tại đây",),
+                            ))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Giao dịch gần đây", style: TextStyle(color: Colors.white54),),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text("Xem tất cả", style: TextStyle(color: Colors.green),)
+                        )
+                      ],
+                    ),
+                    const Card(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Icon(Icons.trending_down, color: Colors.red,),
+                                Text("Tổng số tiền chi", style: TextStyle(color: Colors.white),),
+                                Text("0 đ", style: TextStyle(color: Colors.red),)
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Icon(Icons.trending_up, color: Colors.green,),
+                                Text("Tổng số tiền thu", style: TextStyle(color: Colors.white),),
+                                Text("0 đ", style: TextStyle(color: Colors.green),)
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               )
             ],
           ),
         ),
-      )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.home),
+              ),
+              label: "Tổng quan"
+          ),
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.account_balance_wallet),
+              ),
+              label: "Sổ giao dịch",
+          ),
+          BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.only(top: 18.0),
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => NewTransaction())
+                    );
+                  },
+                  icon: Icon(Icons.add, size: 36,),
+                ),
+              ),
+              label: "",
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.paid),
+              label: "Ngân sách"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Tài khoản"
+          )
+        ],
+        selectedItemColor: Colors.white54,
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.white54,
+      ),
     );
   }
 
